ba66f065eae43b265d9597a04f8f41c7c8a79c64
cmolander <c.molander@hotmail.com>
2018-05-06 12:02:43 +0200

    Checking if answer is right on submit

--- quizlabb/ClientApp/components/Quiz.tsx
+++ quizlabb/ClientApp/components/Quiz.tsx
@@ -30,39 +30,28 @@ export class Quiz extends React.Component<IQuizProps, IQuizState> {
         let question = oldList.map(x => x.question);
         let option1 = oldList.map(o => o.option1);
         let option2 = oldList.map(o => o.option2);
-        let option3 = oldList.map(o => o.option3);    
+        let option3 = oldList.map(o => o.option3);
 
         let counter = this.state.counter;
 
         return (<ol>
             {question[counter]}
-            <br/>
-            <input type="radio" name="q1" value={option1[counter]} />{option1[counter]}<br />
-            <input type="radio" name="q1" value={option2[counter]} />{option2[counter]}<br />
-            <input type="radio" name="q1" value={option3[counter]} />{option3[counter]}<br />
-            <button value="submit" onClick={this.handleSubmit}>Submit</button>
+            <br />
+            <input type="radio" name="q1" value={option1[counter]} onChange={this.handleChange} checked={true}/> {option1[counter]} < br />
+            <input type="radio" name="q1" value={option2[counter]} onChange={this.handleChange} />{option2[counter]}<br />
+            <input type="radio" name="q1" value={option3[counter]} onChange={this.handleChange} />{option3[counter]}<br />
+            <button value="submit" disabled={this.state.isButtonDisable} onClick={this.handleSubmit}>Submit</button>
         </ol>);
 
     }
 
 
-    handleChange(event:any) {
+    handleChange(event: any) {
         this.setState({ selectedOption: event.target.value })
-        this.setState({ isButtonDisable: true })
+        this.setState({ isButtonDisable: false })
 
-        let selected = event.target.value;
 
-        let correctAnswers = this.state.questions.filter(x => x.correctAnswer == selected);
 
-        if (correctAnswers.length > 0) {
-            console.log('1 poäng!');
-        } else {
-            console.log('0 poäng');
-        }
-       
-        correctAnswers = [];
-        console.log(event.target.value)
-      
     }
 
     handleSubmit(event: any) {
@@ -71,12 +60,21 @@ export class Quiz extends React.Component<IQuizProps, IQuizState> {
 
         this.setState({ counter: count })
 
+    
+        let finalSelectedOption = this.state.selectedOption;
 
 
+        let correctAnswers = this.state.questions.filter(x => x.correctAnswer == finalSelectedOption);
+
+        if (correctAnswers.length > 0) {
+            console.log('1 poäng!');
+        } else {
+            console.log('0 poäng');
+        }
+
+        console.log(finalSelectedOption);
         console.log(this.state.counter);
     }
-
-
 
     fetchQuestion() {
         // fråga API:et efter aktuell data
