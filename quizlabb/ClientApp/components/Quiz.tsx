5aa3c287ebb9d58909985785fa6a9f8d44df7304
cmolander <c.molander@hotmail.com>
2018-05-14 00:42:57 +0200

    Added next question button

--- quizlabb/ClientApp/components/Quiz.tsx
+++ quizlabb/ClientApp/components/Quiz.tsx
@@ -8,11 +8,14 @@ interface IQuizProps { }
 interface IQuizState {
     questions: Quest[];
     selectedOption: string;
-    isButtonDisable: boolean;
+    isSubmitButtonDisable: boolean;
+    isNextQuestionButtonDisable: boolean;
+    isOptionDisable: boolean;
     counter: number;
     score: number;
     startQuiz: boolean;
-    correction: string;
+    answerColor: string;
+    showAnswer: string;
 }
 
 export class Quiz extends React.Component<IQuizProps, IQuizState> {
@@ -22,10 +25,11 @@ export class Quiz extends React.Component<IQuizProps, IQuizState> {
 
     public constructor(props: IQuizProps) {
         super(props); {
-            this.state = { questions: [], selectedOption: "", isButtonDisable: true, counter: 0, score: 0, startQuiz: false, correction: "" };
+            this.state = { questions: [], selectedOption: "", isSubmitButtonDisable: true, isNextQuestionButtonDisable: true, isOptionDisable: false, counter: 0, score: 0, startQuiz: false, answerColor: 'white', showAnswer: "" }
         }
         this.fetchQuestion = this.fetchQuestion.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleNextQuestion = this.handleNextQuestion.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.submitScore = this.submitScore.bind(this);
         this.startQuiz = this.startQuiz.bind(this);
@@ -59,6 +63,8 @@ export class Quiz extends React.Component<IQuizProps, IQuizState> {
                 return < div className="text-center"><h1>You got {this.state.score} of {this.state.questions.length} points. Do you want to play again?</h1><button id="startQuizButton" className="submitBtn" onClick={this.restartQuiz}>Start</button></div>
 
             }
+
+
             else {
 
 
@@ -67,22 +73,25 @@ export class Quiz extends React.Component<IQuizProps, IQuizState> {
                         <h2>{question[counter]}</h2>
                         <br />
                         <label className="container">{option1[counter]}
-                            <input type="radio" name="q1" value={option1[counter]} onChange={this.handleChange} checked={this.state.selectedOption === option1[counter]} /> < br />
+                            <input type="radio" name="q1" value={option1[counter]} onChange={this.handleChange} checked={this.state.selectedOption === option1[counter]} disabled={this.state.isOptionDisable} /> < br />
                             <span className="checkmark"></span>
                         </label>
                         <label className="container">{option2[counter]}
-                            <input type="radio" name="q1" value={option2[counter]} onChange={this.handleChange} checked={this.state.selectedOption === option2[counter]} /><br />
+                            <input type="radio" name="q1" value={option2[counter]} onChange={this.handleChange} checked={this.state.selectedOption === option2[counter]} disabled={this.state.isOptionDisable} /><br />
                             <span className="checkmark"></span>
                         </label>
                         <label className="container">{option3[counter]}
-                            <input type="radio" name="q1" value={option3[counter]} onChange={this.handleChange} checked={this.state.selectedOption === option3[counter]} /><br />
+                            <input type="radio" name="q1" style={{ background: 'pink' }} value={option3[counter]} onChange={this.handleChange} checked={this.state.selectedOption === option3[counter]} disabled={this.state.isOptionDisable} /><br />
                             <span className="checkmark"></span>
                         </label>
                         <br />
-                        <button value="submit" className="submitBtn" disabled={this.state.isButtonDisable} onClick={this.handleSubmit}>Submit</button>
+                        <input type="button" value="Submit" className="submitBtn" disabled={this.state.isSubmitButtonDisable} onClick={this.handleSubmit}></input>
+                        <input type="button" value="Next question" className="submitBtn" disabled={this.state.isNextQuestionButtonDisable} onClick={this.handleNextQuestion}></input>
+
                     </ol><div id="infoLabel">
+                        <label id="answerBox" style={{ color: this.state.answerColor }}>{this.state.showAnswer} </label><br />
                         <label id="points">{this.state.score} of {this.state.questions.length} points</label>
-                        </div>
+                    </div>
                 </div>);
             }
         }
@@ -97,22 +106,58 @@ export class Quiz extends React.Component<IQuizProps, IQuizState> {
     restartQuiz() {
         this.setState({ startQuiz: true })
         this.setState({ counter: 0 })
-        this.setState({score: 0})
+        this.setState({ score: 0 })
         console.log(this.state.startQuiz)
-        
+
     }
 
     handleChange(event: any) {
         this.setState({ selectedOption: event.target.value })
-        this.setState({ isButtonDisable: false })
-       
+        this.setState({ isSubmitButtonDisable: false })
+
 
 
     }
 
+
     handleSubmit(event: any) {
 
-       
+
+
+        let finalSelectedOption = this.state.selectedOption;
+
+
+        let correctAnswers = this.state.questions.filter(x => x.correctAnswer == finalSelectedOption);
+
+        if (correctAnswers.length > 0) {
+            console.log('1 poäng!');
+
+            let add = this.state.score + 1;
+
+            this.setState({ score: add })
+
+           
+            this.setState({ answerColor: 'green' })
+            this.setState({ showAnswer: 'Correct answer!' })
+        } else {
+            console.log('0 poäng');
+            this.setState({ answerColor: 'red' })
+            this.setState({ showAnswer: 'Wrong answer!' })
+            
+        }
+
+        console.log(finalSelectedOption);
+
+        this.setState({ isSubmitButtonDisable: true })
+        this.setState({ isNextQuestionButtonDisable: false })
+        this.setState({ isOptionDisable: true })
+
+
+    }
+
+    handleNextQuestion(event: any) {
+
+
         let count = this.state.counter + 1;
 
         if (count <= this.state.questions.length) {
@@ -120,26 +165,14 @@ export class Quiz extends React.Component<IQuizProps, IQuizState> {
             this.setState({ counter: count })
 
 
-            let finalSelectedOption = this.state.selectedOption;
-
-
-            let correctAnswers = this.state.questions.filter(x => x.correctAnswer == finalSelectedOption);
-
-            if (correctAnswers.length > 0) {
-                console.log('1 poäng!');
-
-                let add = this.state.score + 1;
-
-                this.setState({ score: add })
-            } else {
-                console.log('0 poäng');
-            }
-
-            console.log(finalSelectedOption);
             console.log(this.state.counter);
 
-            this.setState({ isButtonDisable: true })
+
         }
+
+        this.setState({ isNextQuestionButtonDisable: true })
+        this.setState({ isOptionDisable: false })
+        this.setState({ answerColor: 'white'})
 
 
 
